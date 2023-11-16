c88455c70b108d1559fef5b33ef669c0d2f32ac6
Monde Nel <mondenel1996@gmail.com>
2023-11-16 12:02:10 +0200

    Add weather application component with user input and temperature display

--- weather/src/App.jsx
+++ weather/src/App.jsx
@@ -1,12 +1,27 @@
 import React from 'react'
 import './App.css'
-
+import cold from './assets/cold.jpg'
 
 function App() {
 
   return (
-    <div>
-      hello
+    <div className='app' style={{ backgroundImage: `url(${cold})` }}>
+      <div className="overlay">
+        <div className="container">
+          <div className="section section__inputs">
+            <input type="text" name="city" placeholder="Enter City..." />
+            <button>°C</button>
+          </div>
+
+          <div className="section section__temperature">
+            <div className="icon">
+              <h3>London, GB</h3>
+              <img src="https://openweathermap.org/img/wn/02d@2x.png" alt="weather icon" />
+              <h3>Cloudy</h3>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
